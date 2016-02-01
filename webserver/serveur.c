a2b35822cb6312f091b811902502405026abb522
Mathieu DE BAERDEMAEKER <m.de-baerdemaeker@etudiant.univ-lille1.fr>
2016-02-01 10:27:26 +0100

    Add server with TCP connection

---
+++ webserver/serveur.c
@@ -0,0 +1,28 @@
+#include <stdlib.h>
+#include <stdio.h>
+#include <sys/socket.h>
+#include <netinet/in.h>
+#include <netinet/ip.h> 
+
+int serveur(void){
+    struct sockaddr_in saddr;
+    saddr.sin_family = AF_INET;
+    saddr.sin_port = htons(8080);
+    saddr.sin_addr.s_addr = INADDR_ANY;
+    int socket_serveur;
+
+    if((socket_serveur = socket(AF_INET, SOCK_STREAM, 0)) == -1){
+        perror("Socket error");
+        return EXIT_FAILURE;
+    }
+    if(bind(socket_serveur, (struct sockaddr *) &saddr, sizeof(saddr)) == -1){
+        perror("Bind error");
+        return EXIT_FAILURE;
+    }
+    if(listen(socket_serveur, 10) == -1){
+        perror("Listen error");
+        return EXIT_FAILURE;
+    }
+    
+    return EXIT_SUCCESS;
+}
