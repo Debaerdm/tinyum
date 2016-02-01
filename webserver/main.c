c1829744906f21f17907580edb185c3428560060
boinc <clement.boin@etudiant.univ-lille1.fr>
2016-02-01 13:26:31 +0100

    Chapter 1 done :-)

--- webserver/main.c
+++ webserver/main.c
@@ -6,18 +6,44 @@
 #include <sys/types.h> 
 #include <sys/socket.h>
 
+#define BUFFER_SIZE 1024
+
 int main(void)
 {
-    int socket_client; 
-    int socket_serveur = create_server(8080);
+  
+    int socket_server;
+    if ((socket_server = create_server(8080)) == -1)
+	return EXIT_FAILURE;
 
-    if ((socket_client = accept(socket_serveur, NULL, NULL)) == -1) {
+    int socket_client;
+    if ((socket_client = accept(socket_server, NULL, NULL)) == -1) {
         perror("Connection refused");
         return EXIT_FAILURE;
     }
 
     const char *message = "Welcome to tinyum, tinyum is a server for TCP connection\n";
-    write(socket_client, message, strlen(message)); 
+    write(socket_client, message, strlen(message));
+
+    char buf[BUFFER_SIZE];
+    int n;
+    for ( ; ; ) {
+      
+	bzero(buf, BUFFER_SIZE);
+      
+	n = read(socket_client, buf, BUFFER_SIZE - 1);
+	if (n == -1) {
+	    perror("read");
+	    return EXIT_FAILURE;
+	}
+	
+	buf[BUFFER_SIZE -1] = '\0';
+	printf("%s", buf);
+	write(socket_client, buf, n);
+
+    }
+
+    close(socket_server);
+    close(socket_client);
 
     return EXIT_SUCCESS;
 }
