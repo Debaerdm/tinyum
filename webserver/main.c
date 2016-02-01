0a3118628a19ef1d582de820636b9e830a7bec6e
Mathieu DE BAERDEMAEKER <m.de-baerdemaeker@etudiant.univ-lille1.fr>
2016-02-01 11:55:30 +0100

    Fix error with client/server

--- webserver/main.c
+++ webserver/main.c
@@ -1,15 +1,23 @@
 #include <stdio.h>
+#include <string.h>
+#include <stdlib.h>
+#include <unistd.h>
 #include "socket.h"
 #include <sys/types.h> 
 #include <sys/socket.h>
 
 int main(void)
 {
-    int socket_serveur = create_server(8080); 
-    char *message = "Welcome to tinyum, tinyum is a server for TCP connection\n";
-    if(accept(socket_serveur, NULL, NULL) == -1){
+    int socket_client; 
+    int socket_serveur = create_server(8080);
+
+    if ((socket_client = accept(socket_serveur, NULL, NULL)) == -1) {
         perror("Connection refused");
         return EXIT_FAILURE;
     }
+
+    const char *message = "Welcome to tinyum, tinyum is a server for TCP connection\n";
+    write(socket_client, message, strlen(message)); 
+
     return EXIT_SUCCESS;
 }
--- webserver/socket.c
+++ webserver/socket.c
@@ -1,28 +1,31 @@
 #include <stdlib.h>
+#include <unistd.h>
 #include <stdio.h>
 #include <sys/socket.h>
 #include <netinet/in.h>
 #include <netinet/ip.h> 
 
-int create_server(int port){
+int create_server(int port)
+{
+    int socket_server;
     struct sockaddr_in saddr;
     saddr.sin_family = AF_INET;
     saddr.sin_port = htons(port);
     saddr.sin_addr.s_addr = INADDR_ANY;
-    int socket_serveur;
-
-    if((socket_serveur = socket(AF_INET, SOCK_STREAM, 0)) == -1){
+    
+    if ((socket_server = socket(AF_INET, SOCK_STREAM, 0)) == -1) {
         perror("Socket error");
         return EXIT_FAILURE;
     }
-    if(bind(socket_serveur, (struct sockaddr *) &saddr, sizeof(saddr)) == -1){
+
+    if (bind(socket_server, (struct sockaddr *) &saddr, sizeof(saddr)) == -1) {
         perror("Bind error");
         return EXIT_FAILURE;
     }
-    if(listen(socket_serveur, 10) == -1){
+
+    if (listen(socket_server, 10) == -1) {
         perror("Listen error");
         return EXIT_FAILURE;
     }
-    
-    return socket_serveur;
+    return socket_server;
 }
