ef17236230ec6f723e728b12a36e0b0bf5bc9564
boinc <clement.boin@etudiant.univ-lille1.fr>
2016-02-01 16:33:20 +0100

    Clean the buffer for the next msg

--- webserver/main.c
+++ webserver/main.c
@@ -11,38 +11,40 @@
 int main(void)
 {
 
-		int socket_server;
-		if ((socket_server = create_server(8080)) == -1)
-				return EXIT_FAILURE;
+    int socket_server;
+    if ((socket_server = create_server(8080)) == -1) return EXIT_FAILURE;
 
-		int socket_client;
-		if ((socket_client = accept(socket_server, NULL, NULL)) == -1) {
-				perror("Connection refused");
-				return EXIT_FAILURE;
-		}
+    int socket_client;
+    if ((socket_client = accept(socket_server, NULL, NULL)) == -1) {
+	perror("Connection refused");
+	return EXIT_FAILURE;
+    }
 
-		const char *message = "Welcome to tinyum, tinyum is a server for TCP connection\n";
-		write(socket_client, message, strlen(message));
+    const char *message = "Welcome to tinyum, tinyum is a server for TCP connection\n";
+    if (write(socket_client, message, strlen(message)) == -1) {
+	perror("write");
+	return EXIT_FAILURE;
+    }
 
-		char buf[BUFFER_SIZE];
-		int n;
-		for ( ; ; ) {
+    char buf[BUFFER_SIZE];
+    int n;
+    for ( ; ; ) {
 
-				bzero(buf, BUFFER_SIZE);
+	if ((n = read(socket_client, buf, BUFFER_SIZE - 1)) == -1) {
+	    perror("read");
+	    return EXIT_FAILURE;
+	}
 
-				if ((n = read(socket_client, buf, BUFFER_SIZE - 1)) == -1) {
-						perror("read");
-						return EXIT_FAILURE;
-				}
+	buf[BUFFER_SIZE -1] = '\0';
+	printf("%s", buf);
+	write(socket_client, buf, n);
 
-				buf[BUFFER_SIZE -1] = '\0';
-				printf("%s", buf);
-				write(socket_client, buf, n);
+	/* clean the current buffer */
+	memset(buf, '\0', BUFFER_SIZE);
+    }
 
-		}
+    close(socket_server);
+    close(socket_client);
 
-		close(socket_server);
-		close(socket_client);
-
-		return EXIT_SUCCESS;
+    return EXIT_SUCCESS;
 }
