8a7822b7f90a9b1bbe0fc9cc6200423de71476e7
Debaerdm <mathieu.debaerdemaeker@gmail.com>
2016-02-03 23:20:21 +0100

    Rename signal to signals

--- webserver/client.c
+++ webserver/client.c
@@ -27,7 +27,7 @@
 #include <sys/types.h> 
 #include <sys/socket.h>
 #include <sys/wait.h>
-#include "signal.h"
+:x
 #include "socket.h"
 
 
--- webserver/signals.h
+++ webserver/signals.h
@@ -1,5 +1,5 @@
-#ifndef __SIGNAL_H__
-#define __SIGNAL_H__
+#ifndef __SIGNALS_H__
+#define __SIGNALS_H__
 	void initialize_signals(void);
 	void traitement_signal(int sig);
 	void initialiser_signaux(void);
