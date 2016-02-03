37f68e41837bb7f6b8d5bc39f7a2bb20dc18b1f8
Mathieu DE BAERDEMAEKER <m.de-baerdemaeker@etudiant.univ-lille1.fr>
2016-02-03 17:28:50 +0100

    Correction of memory Error and add multi-connecting client

--- webserver/main.c
+++ webserver/main.c
@@ -1,10 +1,34 @@
+/*
+ * webserver/main.c
+ * Client for connecting in the socket server
+ *
+ * Copyright (C) 2016 Mathieu DE BAERDEMAEKER mathieu.debaerdemaeker@gmail.com
+ *
+ * This program is free software; you can redistribute it and/or modify
+ * it under the terms of the GNU General Public License as published by
+ * the Free Software Foundation; either version 2 of the License, or (at
+ * your option) any later version.
+ *
+ * This program is distributed in the hope that it will be useful, but
+ * WITHOUT ANY WARRANTY; without even the implied warranty of
+ * MERCHANTABILITY OR FITNESS FOR A PARTICULAR PURPOSE, GOOD TITLE or
+ * NON INFRINGEMENT.  See the GNU General Public License for more
+ * details.
+ *
+ * You should have received a copy of the GNU General Public License
+ * along with this program; if not, write to the Free Software
+ * Foundation, Inc., 675 Mass Ave, Cambridge, MA 02139, USA.
+ */
+
 #include <stdio.h>
 #include <string.h>
 #include <stdlib.h>
 #include <unistd.h>
 #include <sys/types.h> 
 #include <sys/socket.h>
+#include <sys/wait.h>
 #include "socket.h"
+
 
 #define BUFFER_SIZE 1024
 
@@ -14,38 +38,51 @@ int main(void)
     int socket_server;
     if ((socket_server = create_server(8080)) == -1) return EXIT_FAILURE;
 
-    int socket_client;
-    if ((socket_client = accept(socket_server, NULL, NULL)) == -1) {
-	perror("Connection refused");
-	return EXIT_FAILURE;
+    for( ; ; ){
+        int socket_client;
+        if ((socket_client = accept(socket_server, NULL, NULL)) == -1) {
+            perror("Connection refused");
+            return EXIT_FAILURE;
+        }
+
+        pid_t pid;
+        if((pid = fork()) == -1){
+            perror("fork");
+            return EXIT_FAILURE;
+        }
+
+        if(pid == 0){
+            initialize_signals();
+            const char *message = "Welcome to tinyum, tinyum is a server for TCP connection\n";
+            if (write(socket_client, message, strlen(message)) == -1) {
+                perror("write");
+                return EXIT_FAILURE;
+            }
+            char buf[BUFFER_SIZE];
+            int n;
+            for (;;) {
+
+                /* clean the current buffer */
+                memset(buf, 0, BUFFER_SIZE);
+
+                if ((n = read(socket_client, buf, BUFFER_SIZE - 1)) == -1) {
+                    perror("read");
+                    return EXIT_FAILURE;
+                }
+
+                buf[BUFFER_SIZE -1] = '\0';
+                printf("%s", buf);
+                write(socket_client, buf, n);
+
+            }
+
+        }
+        else
+            close(socket_client);
+        /*int status;
+        (void)waitpid(pid, &status, 0);*/
     }
-
-    const char *message = "Welcome to tinyum, tinyum is a server for TCP connection\n";
-    initialize_signals();
-    if (write(socket_client, message, strlen(message)) == -1) {
-	perror("write");
-	return EXIT_FAILURE;
-    }
-
-    char buf[BUFFER_SIZE];
-    int n;
-    for ( ; ; ) {
-
-	if ((n = read(socket_client, buf, BUFFER_SIZE - 1)) == -1) {
-	    perror("read");
-	    return EXIT_FAILURE;
-	}
-
-	buf[BUFFER_SIZE -1] = '\0';
-	printf("%s", buf);
-	write(socket_client, buf, n);
-
-	/* clean the current buffer */
-	memset(buf, '\0', BUFFER_SIZE);
-    }
-
     close(socket_server);
-    close(socket_client);
 
     return EXIT_SUCCESS;
 }
