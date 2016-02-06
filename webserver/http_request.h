aada032b11dc08e532a099284a97be45d0606e3a
cboin <clem.boin@gmail.com>
2016-02-06 15:15:30 +0100

    type def http_request

---
+++ webserver/http_request.h
@@ -0,0 +1,20 @@
+enum http_method {
+     HTTP_OPTIONS,
+     HTTP_GET,
+     HTTP_HEAD,
+     HTTP_POST,
+     HTTP_PUT,
+     HTTP_DELETE,
+     HTTP_TRACE,
+     HTTP_CONNECT,
+     HTTP_INVALID
+};
+
+/* Request-Line = Method SP Request-URI SP HTTP-Version CRLF */
+typedef struct {
+     enum http_method m;
+     int major_version;
+     int minor_version;
+     char* uri;
+} http_request;
+     
