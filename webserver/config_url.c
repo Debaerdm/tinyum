efffaf94b7cf8f18eb05832be3589e0cab479fe3
Clément Boin <clem.boin@gmail.com>
2016-03-07 14:23:47 +0100

    Add get_file_size()

--- webserver/config_url.c
+++ webserver/config_url.c
@@ -6,12 +6,12 @@
 #include <fcntl.h>
 
 const char  *rewrite_url(char *url){
-    if(strcmp(url, "/") == 0){
+    if(strcmp(url, "/") == 0) {
 	return "/index.html";
     }
     printf("%s\n", url);
     char *tmp = strchr(url, '?');
-    if(tmp == NULL){
+    if (tmp == NULL) {
 	return url;
     }
     url[(tmp - url)] = '\0';
@@ -19,12 +19,23 @@ const char  *rewrite_url(char *url){
 }
 
 int check_and_open(const char *url, const char *document_root){
-    char path[strlen(url)+strlen(document_root)+1];
+    char path[strlen(url) + strlen(document_root) + 1];
     sprintf(path, "%s%s", url, document_root);
     struct stat conf;
     stat(path, &conf);
-    if(S_ISREG(conf.st_mode)){
+    if (S_ISREG(conf.st_mode)) {
 	return open(path, O_RDONLY);
     }
-    return -1;
+    return EXIT_FAILURE;
+}
+
+int get_file_size(int fildes) {
+    struct stat file_stat;
+
+    if (fstat(fildes, &file_stat) < 0) {
+        perror("fstat");
+        return EXIT_FAILURE;
+    }
+
+    return file_stat.st_size;
 }
