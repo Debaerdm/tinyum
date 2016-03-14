0f9ba0e1aea14cd17700e0a72e57310299ac9efc
Debaerdm <mathieu.debaerdemaeker@gmail.com>
2016-03-14 21:19:06 +0100

    Put public_html in your home

--- webserver/public_html/main.js
+++ webserver/public_html/main.js
@@ -2,11 +2,14 @@ $(document).ready(function(){
     $("#flip").click(function(){
 	$("#panel").slideToggle("slow");
     });
+    
     $("#object").mouseenter(function(){
 	$("#object li").css("font-size",  "24px");
     });
+    
     $("#object").mouseenter(function(){
 	$("#object li").css("font-size",  "16px");
     });
+    
 });
 
