812555b4ee1fc490a2d82a3e076998cebcdd0745
Debaerdm <mathieu.debaerdemaeker@gmail.com>
2016-03-21 23:10:11 +0100

    Implementation and test of semaphore .... failed

--- webserver/src/main.c
+++ webserver/src/main.c
@@ -6,18 +6,23 @@
 #include <sys/socket.h>
 #include <sys/wait.h>
 #include <setjmp.h>
+#include <semaphore.h>
 #include "../include/signals.h"
 #include "../include/socket.h"
 #include "../include/stats.h"
 
 #define handle_error(msg) \
-	do { perror(msg); exit(EXIT_FAILURE); } while (0)
+  do { perror(msg); return EXIT_FAILURE;  } while (0)
 
 int main(void)
 {
   int socket_client, socket_server;
   if ((socket_server = create_server(8080)) == -1) return EXIT_FAILURE;
-
+  
+  sem_t mutex;
+  if(sem_init(&mutex, 0,1) < 0 ){
+    handle_error("Semaphore init");
+  }
   initialize_signals();
   init_stats();
   web_stats *stats = get_stats();
@@ -40,16 +45,23 @@ int main(void)
 	handle_error("fork");
     }
     if(pid == 0){
+      sem_wait(&mutex);
       if(client(tinyum, socket_client) == 1)
 	handle_error("Communication");
+      sem_post(&mutex);
       return EXIT_SUCCESS;
     }
     else {
+      sem_wait(&mutex);
       fclose(tinyum);
       close(socket_client);
+      sem_post(&mutex);
     }
   }
 
+  if(sem_destroy(&mutex) < 0){
+    handle_error("Semaphrore destroy");
+  }
   close(socket_server);
 
   return EXIT_SUCCESS;
